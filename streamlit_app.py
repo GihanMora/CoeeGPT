628ec0d0dc6f06dac1c3c15d05ba0940ae79ae6f
Gihan Gamage <gihangamage.15@cse.mrt.ac.lk>
2023-04-19 19:16:24 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -181,46 +181,46 @@ elif uploaded_file:
             col2.error('Oops, the GPT response resulted in an error :( Please try again with a different question.')
 
             
-def generate_response(prompt):
-    completions = openai.Completion.create(
-        engine = "text-davinci-003",
-        prompt = prompt,
-        max_tokens = 1024,
-        n = 1,
-        stop = None,
-        temperature=0.5,
-    )
-    message = completions.choices[0].text
-    return message 
+# def generate_response(prompt):
+#     completions = openai.Completion.create(
+#         engine = "text-davinci-003",
+#         prompt = prompt,
+#         max_tokens = 1024,
+#         n = 1,
+#         stop = None,
+#         temperature=0.5,
+#     )
+#     message = completions.choices[0].text
+#     return message 
 
-buff, col, buff2 = st.beta_columns([1,3,1])
+# buff, col, buff2 = st.beta_columns([1,3,1])
 
-#Creating the chatbot interface
-col.title("chatBot : Streamlit + openAI")
+# #Creating the chatbot interface
+# col.title("chatBot : Streamlit + openAI")
 
-# Storing the chat
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = []
+# # Storing the chat
+# if 'generated' not in st.session_state:
+#     st.session_state['generated'] = []
 
-if 'past' not in st.session_state:
-    st.session_state['past'] = []
+# if 'past' not in st.session_state:
+#     st.session_state['past'] = []
 
-# We will get the user's input by calling the get_text function
-def get_text():
-    input_text = col.text_input("You: ","Hello, how are you?", key="input")
-    return input_text
+# # We will get the user's input by calling the get_text function
+# def get_text():
+#     input_text = col.text_input("You: ","Hello, how are you?", key="input")
+#     return input_text
 
 
-user_input = get_text()
+# user_input = get_text()
 
-if user_input:
-    output = generate_response(user_input)
-    # store the output 
-    st.session_state.past.append(user_input)
-    st.session_state.generated.append(output)
+# if user_input:
+#     output = generate_response(user_input)
+#     # store the output 
+#     st.session_state.past.append(user_input)
+#     st.session_state.generated.append(output)
     
-if st.session_state['generated']:
+# if st.session_state['generated']:
     
-    for i in range(len(st.session_state['generated'])-1, -1, -1):
-        message(st.session_state["generated"][i], key=str(i))
-        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+#     for i in range(len(st.session_state['generated'])-1, -1, -1):
+#         message(st.session_state["generated"][i], key=str(i))
+#         message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
